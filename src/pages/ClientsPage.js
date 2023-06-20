0d8e56e3ff816b5d1df5dbcc8863e08d47375ece
Jose Aparicio <jfacademusic@gmail.com>
2023-06-20 17:22:02 -0400

    mobile page integrated

--- src/pages/ClientsPage.js
+++ src/pages/ClientsPage.js
@@ -1,8 +1,21 @@
+import Clients from '../assets/Clients.json'
+
 function ClientsPage() {
     return (
-        <div>
-
-        </div>
+        <main>
+            <section className='flex justify-center items-center bg-black pt-[1rem]'>
+                <h1 className='text-[2rem] font-bold text-white'>CLIENTS</h1>
+            </section>
+            <section className='flex flex-col justify-start items-start p-[1.5rem] bg-black'>
+                {
+                    Clients.map((client, index) => {
+                        return (
+                            <img src={client.photo} key={index} alt={client.name} className='w-[100%] h-[40vw]'/>
+                        )
+                    })
+                }
+            </section>
+        </main>
     )
 }
 
