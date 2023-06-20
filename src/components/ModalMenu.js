9ea4b2ac97cdf5df10772172f7f6be9b51d8f28d
Jose Aparicio <jfacademusic@gmail.com>
2023-06-20 05:44:26 -0400

    added modal styling

--- src/components/ModalMenu.js
+++ src/components/ModalMenu.js
@@ -4,44 +4,39 @@ import { Link } from 'react-router-dom';
 
 function ModalMenu({ toggleModal }) {
     return ReactDom.createPortal(
-        <div className='bg-black/[0.5] fixed top-0 left-0 right-0 bottom-0 z-10'>
+        <div className='bg-black/[0.7] fixed top-0 left-0 right-0 bottom-0 z-10'>
             <div className="fixed top-0 left-0 w-[100%] h-[100%] z-10">
                 <svg
                     onClick={toggleModal}
-                    className='absolute top-[0.6rem] right-[0.5rem]'
-                    width="75px"
-                    height="75px"
+                    className='absolute top-[0.6rem] right-[0.5rem] cursor-pointer w-[70px] h-[70px] stroke-current text-white hover:text-light-blue'
                     viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <g id="Menu / Close_SM">
-                        <path id="Vector" d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                    </g>
+                    >
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m16 16-4-4m0 0L8 8m4 4 4-4m-4 4-4 4" />
                 </svg>
-                <nav className='text-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                    <ul className='flex flex-col gap-y-[1.5rem]'>
+                <nav className='text-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-[500] text-[1.5rem]'>
+                    <ul className='flex flex-col gap-y-[1.5rem] text-center '>
                         <Link to="/" onClick={toggleModal}>
-                            <li>HOME</li>
+                            <li className='hover:text-light-blue'>HOME</li>
                         </Link>
                         <Link to="/services" onClick={toggleModal}>
-                            <li>SERVICES</li>
+                            <li className='hover:text-light-blue'>SERVICES</li>
                         </Link>
                         <Link>
-                            <li>STUDIOS</li>
+                            <li className='hover:text-light-blue'>STUDIOS</li>
                         </Link>
                         <Link>
-                            <li>THE TEAM</li>
+                            <li className='hover:text-light-blue'>THE TEAM</li>
                         </Link>
                         <Link>
-                            <li>CLIENTS</li>
+                            <li className='hover:text-light-blue'>CLIENTS</li>
                         </Link>
                         <Link>
-                            <li>CONTACT US</li>
+                            <li className='hover:text-light-blue'>CONTACT US</li>
                         </Link>
                     </ul>
                 </nav>
             </div>
-        </div>,
+        </div >,
         document.getElementById('portal')
     )
 }
