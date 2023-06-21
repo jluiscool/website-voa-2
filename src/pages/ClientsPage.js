f0453291ad92cc27b643b613f874c621071e8047
Jose Aparicio <jfacademusic@gmail.com>
2023-06-21 08:40:09 -0400

    minor styling to clients page

--- src/pages/ClientsPage.js
+++ src/pages/ClientsPage.js
@@ -6,11 +6,11 @@ function ClientsPage() {
             <section className='flex justify-center items-center bg-black pt-[1rem]'>
                 <h1 className='text-[2rem] font-bold text-white'>CLIENTS</h1>
             </section>
-            <section className='flex flex-col justify-start items-start p-[1.5rem] bg-black'>
+            <section className='flex flex-col justify-start items-start p-[1.5rem] bg-black gap-y-[1rem]'>
                 {
                     Clients.map((client, index) => {
                         return (
-                            <img src={client.photo} key={index} alt={client.name} className='w-[100%] h-[40vw]'/>
+                            <img src={client.photo} key={index} alt={client.name} className='w-[100%] h-[40vw] border-light-blue border-[3px]'/>
                         )
                     })
                 }
