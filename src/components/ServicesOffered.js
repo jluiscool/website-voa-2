6f59f0585437aa9ba2d162b4078518e72b12e835
Jose Aparicio <jfacademusic@gmail.com>
2023-06-21 18:41:25 -0400

    homepage rentals section tablet responsiveness added

--- src/components/ServicesOffered.js
+++ src/components/ServicesOffered.js
@@ -10,56 +10,56 @@ import VideoPostService from '../assets/images/video-post-service.svg';
 
 function ServicesOffered() {
     return (
-        <div className='bg-hot-pink w-[100%] flex flex-wrap justify-around p-[24px] gap-x-[16px] gap-y-[4.5vw]'>
-            <div className='w-[29%] h-[3rem] flex flex-col items-center justify-between gap-y-[0.5rem] '>
+        <div className='bg-hot-pink w-[100%] flex flex-wrap justify-between p-[24px] tablet:p-[2rem] tablet:py-[3rem] gap-x-[16px] gap-y-[4.5vw]'>
+            <div className='w-[29%] h-[3rem] tablet:h-[auto] tablet:justify-between flex flex-col items-center justify-between gap-y-[0.5rem]'>
                 <img src={TranslationService} className='h-[5vw]' alt="Translation Service" />
                 <button className='w-[90%] m-w-[9.375rem] bg-transparent border-black border-[1px] rounded-[12px] py-[.25rem] translation-service'>
                     <p className="text-[8px]">TRANSLATION</p>
                 </button>
             </div>
-            <div className='w-[29%] h-[3rem] flex flex-col items-center justify-between gap-y-[0.5rem]  dubbing-service'>
+            <div className='w-[29%] h-[3rem] tablet:h-[auto] tablet:justify-between flex flex-col items-center justify-between gap-y-[0.5rem]  dubbing-service'>
                 <img src={DubbingService} className='h-[5vw]' alt="Dubbing Service" />
                 <button className='w-[90%] m-w-[9.375rem] bg-transparent border-black border-[1px] rounded-[12px] py-[.25rem] dubbing-service'>
                     <p className="text-[8px]">DUBBING</p>
                 </button>
             </div>
-            <div className='w-[29%] h-[3rem] flex flex-col items-center justify-between gap-y-[0.5rem]  voice-over-service'>
+            <div className='w-[29%] h-[3rem] tablet:h-[auto] tablet:justify-between flex flex-col items-center justify-between gap-y-[0.5rem]  voice-over-service'>
                 <img src={VoiceOverService} className='h-[5vw]' alt="Voice Over Service" />
                 <button className='w-[90%] m-w-[9.375rem] bg-transparent border-black border-[1px] rounded-[12px] py-[.25rem] voice-over-service'>
                     <p className="text-[8px]">VOICE OVER</p>
                 </button>
             </div>
-            <div className='w-[29%] h-[3rem] flex flex-col items-center justify-between gap-y-[0.5rem]  recording-service'>
+            <div className='w-[29%] h-[3rem] tablet:h-[auto] tablet:justify-between flex flex-col items-center justify-between gap-y-[0.5rem]  recording-service'>
                 <img src={RecordingService} className='h-[5vw]' alt="Recording Service" />
                 <button className='w-[90%] m-w-[9.375rem] bg-transparent border-black border-[1px] rounded-[12px] py-[.25rem] recording-service'>
                     <p className="text-[8px]">RECORDING</p>
                 </button>
             </div>
-            <div className='w-[29%] h-[3rem] flex flex-col items-center justify-between gap-y-[0.5rem]  mixing-service'>
+            <div className='w-[29%] h-[3rem] tablet:h-[auto] tablet:justify-between flex flex-col items-center justify-between gap-y-[0.5rem]  mixing-service'>
                 <img src={MixingService} className='h-[5vw]' alt="Mixing Service" />
                 <button className='w-[90%] m-w-[9.375rem] bg-transparent border-black border-[1px] rounded-[12px] py-[.25rem] mixing-service'>
                     <p className="text-[8px]">MIXING</p>
                 </button>
             </div>
-            <div className='w-[29%] h-[3rem] flex flex-col items-center justify-between gap-y-[0.5rem]  mastering-service'>
+            <div className='w-[29%] h-[3rem] tablet:h-[auto] tablet:justify-between flex flex-col items-center justify-between gap-y-[0.5rem]  mastering-service'>
                 <img src={MasteringService} className='h-[5vw]' alt="Mastering Service" />
                 <button className='w-[90%] m-w-[9.375rem] bg-transparent border-black border-[1px] rounded-[12px] py-[.25rem] mastering-service'>
                     <p className="text-[8px]">MASTERING</p>
                 </button>
             </div>
-            <div className='w-[29%] h-[3rem] flex flex-col items-center justify-between gap-y-[0.5rem]  sound-design-service'>
+            <div className='w-[29%] h-[3rem] tablet:h-[auto] tablet:justify-between flex flex-col items-center justify-between gap-y-[0.5rem]  sound-design-service'>
                 <img src={SoundDesignService} className='h-[5vw]' alt="Sound Design Service" />
                 <button className='w-[90%] m-w-[9.375rem] bg-transparent border-black border-[1px] rounded-[12px] py-[.25rem] sound-design-service'>
                     <p className="text-[8px]">SOUND DESIGN</p>
                 </button>
             </div>
-            <div className='w-[29%] h-[3rem] flex flex-col items-center justify-between gap-y-[0.5rem]  music-service'>
+            <div className='w-[29%] h-[3rem] tablet:h-[auto] tablet:justify-between flex flex-col items-center justify-between gap-y-[0.5rem]  music-service'>
                 <img src={MusicService} className='h-[5vw]' alt="Music Service" />
                 <button className='w-[90%] m-w-[9.375rem] bg-transparent border-black border-[1px] rounded-[12px] py-[.25rem] music-service'>
                     <p className="text-[8px]">MUSIC</p>
                 </button>
             </div>
-            <div className='w-[29%] h-[3rem] flex flex-col items-center justify-between gap-y-[0.5rem]  video-post-service'>
+            <div className='w-[29%] h-[3rem] tablet:h-[auto] tablet:justify-between flex flex-col items-center justify-between gap-y-[0.5rem]  video-post-service'>
                 <img src={VideoPostService} className='h-[5vw]' alt="Video Post Production Service" />
                 <button className='w-[90%] m-w-[9.375rem] bg-transparent border-black border-[1px] rounded-[12px] py-[.25rem] video-post-service'>
                     <p className="text-[8px]">VIDEO POST</p>
--- src/components/StudioRentals.js
+++ src/components/StudioRentals.js
@@ -9,18 +9,18 @@ function StudioRentals({ newHeight, handleScroll }) {
     }
 
     return (
-        <div className={`bg-[url("./assets/images/microphone-studio-rentals.jpg")] bg-cover bg-no-repeat w-[100%] flex flex-col items-center p-[1.5rem] ${newHeight ? `h-[${newHeight}vh] justify-center` : ""}`}>
-            <h2 className='text-light-blue mb-[1rem] font-[700] bg-black/[0.5] rounded-[12px] px-[0.5rem]'>
+        <div className={`bg-[url("./assets/images/microphone-studio-rentals.jpg")] bg-cover bg-no-repeat w-[100%] flex flex-col items-start p-[1.5rem] tablet:p-[2rem] ${newHeight ? `h-[${newHeight}vh] justify-center` : "h-[35vw]"} max-h-[150px] tablet:max-h-[200px]`}>
+            <h2 className='text-light-blue mb-[6.5vw] font-[700] bg-black/[0.5] rounded-[12px] px-[0.5rem] mx-auto'>
                 RENTALS
             </h2>
             <div className='flex justify-between w-[100%]'>
-                <button onClick={() => checkScroll("sectionOne")} className='bg-light-blue rounded-[3px] shadow-md shadow-black py-[0.25rem] px-[0.60rem]'>
+                <button onClick={() => checkScroll("sectionOne")} className='bg-light-blue max-w-[175px] w-[30%] rounded-[3px] shadow-md shadow-black py-[0.25rem] px-[0.60rem]'>
                     <p className='text-[0.75rem] font-[500]'>SUNBOX</p>
                 </button>
-                <button onClick={() => checkScroll("sectionTwo")} className='bg-light-blue rounded-[3px] shadow-md shadow-black py-[0.25rem] px-[0.60rem]'>
+                <button onClick={() => checkScroll("sectionTwo")} className='bg-light-blue max-w-[175px] w-[30%] rounded-[3px] shadow-md shadow-black py-[0.25rem] px-[0.60rem]'>
                     <p className='text-[0.75rem] font-[500]'>PURPLEBOX</p>
                 </button>
-                <button onClick={() => checkScroll("sectionThree")} className='bg-light-blue rounded-[3px] shadow-md shadow-black py-[0.25rem] px-[0.60rem]'>
+                <button onClick={() => checkScroll("sectionThree")} className='bg-light-blue max-w-[175px] w-[30%] rounded-[3px] shadow-md shadow-black py-[0.25rem] px-[0.60rem]'>
                     <p className='text-[0.75rem] font-[500]'>MOONBOX</p>
                 </button>
             </div>
